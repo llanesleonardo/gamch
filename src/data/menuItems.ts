243afc9464478345fedefeb86e07f73d93c9d320
llanesleonardo <llanesleonardo@gmail.com>
2025-06-28 00:50:32 -0700

    menu item name changed

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -3,8 +3,8 @@ export const menuItems = [
   { label: "Home", href: "#home" },
   { label: "Event", href: "#event" },
   { label: "Activities", href: "#activities" },
-  { label: "Leadership Team", href: "#leadership" },
-  { label: "Event Registration", href: "#registration" },
+  { label: "Team", href: "#leadership" },
+  { label: "Registration", href: "#registration" },
   { label: "Contact", href: "#contact" },
   { label: "About Salvation Army", href: "#aboutus" },
 ]
